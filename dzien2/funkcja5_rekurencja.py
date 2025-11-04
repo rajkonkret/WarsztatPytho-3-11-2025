22f7895c018acaf299cd1dff799b72ae2df4a4e4
rajkonkret <rajkonkret@gmail.com>
2025-11-04 08:37:10 +0100

    [2 2] add funkcja5_rekurencja.py

---
+++ dzien2/funkcja5_rekurencja.py
@@ -0,0 +1,31 @@
+# funkcja rekurencyjna
+# wywołuje sama siebie
+
+def silnia(n):
+    if n == 0:
+        return 1
+    else:
+        return n * silnia(n - 1)
+
+
+print(silnia(5))  # 120
+
+
+def nwd(a, b):
+    if b == 0:
+        return a
+    return nwd(b, a % b)  # % modulo, reszta z dzielenia
+
+
+print(nwd(48, 18))  # 6
+
+nested_data = {
+    "a": 1,
+    "b": {
+        "c": 2,
+        "d": [3, 4, {"e": 5}]
+    },
+    "f": [6, 7]
+}
+
+# zsumowanie wszystkie wartości z tego słownika
