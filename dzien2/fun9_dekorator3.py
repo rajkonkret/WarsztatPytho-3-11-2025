4cf7247d2f930401789147a413c8991228af6dc6
rajkonkret <rajkonkret@gmail.com>
2025-11-04 11:33:32 +0100

    [2 12] update fun9_dekorator3.py

--- dzien2/fun9_dekorator3.py
+++ dzien2/fun9_dekorator3.py
@@ -5,18 +5,21 @@ import numpy as np
 # numpy - do działań na tablicach i macierzach, napisana w C
 # pip install numpy
 
-tracemalloc.start()
+# tracemalloc.start()
+
 # array1 = np.arange(10_000_000, dtype=np.int64)
-array1 = np.arange(10_000_000, dtype=np.int32)
+array1 = np.arange(10_000_000, dtype=np.int8)
 # array2 = np.arange(10_000_000, dtype=np.int64)
-array2 = np.arange(10_000_000, dtype=np.int32)
-current, peak = tracemalloc.get_traced_memory()
-tracemalloc.stop()
-print(f'Current memory usage: {current / 1024 ** 2} MB')
-print(f'Peak memory usage: {peak / 1024 ** 2} MB')
-print(array1.dtype)  # int64
-print(np.iinfo(np.int64))
-print(np.iinfo(np.int32))
+array2 = np.arange(10_000_000, dtype=np.int8)
+
+# current, peak = tracemalloc.get_traced_memory()
+# tracemalloc.stop()
+# print(f'Current memory usage: {current / 1024 ** 2} MB')
+# print(f'Peak memory usage: {peak / 1024 ** 2} MB')
+# print(array1.dtype)  # int64
+# print(np.iinfo(np.int64))
+# print(np.iinfo(np.int32))
+# print(np.iinfo(np.int8))
 # ---------------------------------------------------------------
 # min = -9223372036854775808
 # max = 9223372036854775807
@@ -26,15 +29,24 @@ print(np.iinfo(np.int32))
 # min = -2147483648
 # max = 2147483647
 # ---------------------------------------------------------------
+# Machine parameters for int8
+# ---------------------------------------------------------------
+# min = -128
+# max = 127
+# ---------------------------------------------------------------
 # Current memory usage: 152.58807373046875 MB
 # Peak memory usage: 152.58807373046875 MB
 # Current memory usage: 76.29412841796875 MB
 # Peak memory usage: 76.29412841796875 MB
+# Current memory usage: 19.07366943359375 MB
+# Peak memory usage: 19.07366943359375 MB
+# int8
 
 #
 # tracemalloc.start()
-# lista1 = list(range(10_000_000))
-# lista2 = list(range(10_000_000))
+
+lista1 = list(range(10_000_000))
+lista2 = list(range(10_000_000))
 #
 # current, peak = tracemalloc.get_traced_memory()
 # tracemalloc.stop()
