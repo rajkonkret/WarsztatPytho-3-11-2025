c1febbfb482cd82c8e8254f62d92f058c146721c
rajkonkret <rajkonkret@gmail.com>
2025-11-04 12:31:40 +0100

    [2 17] update fun11_gen2.py

--- dzien2/fun11_gen2.py
+++ dzien2/fun11_gen2.py
@@ -25,5 +25,51 @@ print(20 * "-")
 for i in wznowienie(6, 8):
     if i == 1001:
         continue
-    time.sleep(1)
+    # time.sleep(1)
     print(f"Yield zwraca wartość: {i}")
+
+print(20 * "-")
+
+
+def gen4():
+    i = 1
+    while True:
+        yield i * i
+        i += 1
+
+
+g4 = gen4()
+print(next(g4))
+print(next(g4))
+print(next(g4))
+print(next(g4))
+print(next(g4))
+print(next(g4))
+
+
+def gen5():
+    i = 1
+    while True:
+        command = yield i * i
+        print(command)
+        if command == "stop":
+            break  # zatrzyma generator
+        i += 1
+
+
+g5 = gen5()
+print(next(g5))
+print(next(g5))
+print(next(g5))
+print(next(g5))
+
+g5.send("OK")  # OK
+
+try:
+    g5.send("stop")  # StopIteration
+except StopIteration:
+    print("Koniec danych")
+except Exception as e:
+    print("Bład:", e)
+# stop
+# Koniec danych
