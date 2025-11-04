0b4d36fcb880d14221b709eabd52172b8b03b53a
rajkonkret <rajkonkret@gmail.com>
2025-11-04 10:36:10 +0100

    [2 8] update fun8_dek2.py

--- dzien2/fun8_dek2.py
+++ dzien2/fun8_dek2.py
@@ -3,12 +3,35 @@
 # funkcja -> zwraca tekst
 # dekorator -> zamienia wynik na duże litery
 
+# pip install colorama
+from colorama import Fore, Style, init
+
+init(autoreset=True)
+
+
 def uppercase_decorator(func):
     def wrapper(*args, **kwargs):
         result = func(*args, **kwargs)  # uruchamiamy funkcję
         return result.upper()
 
     return wrapper  # zwracamy adres, referencje
+
+
+# dekorator, który zmienia wynik na bold, czerwony
+def bold_decorator(func):
+    def wrapper(*args, **kwargs):
+        result = func(*args, **kwargs)
+        return "\033[1m" + result + "\033[0m"
+
+    return wrapper
+
+
+def color_decorator(func):
+    def wrapper(*args, **kwargs):
+        result = func(*args, **kwargs)
+        return Fore.RED + result + Style.RESET_ALL
+
+    return wrapper
 
 
 @uppercase_decorator
@@ -32,11 +55,22 @@ wyn = tekst.upper()
 print(wyn)  # RADEK
 
 
+@bold_decorator
 @uppercase_decorator
 def greeting2(string):
     return f"Podałeś: {string}"
 
 
 print(greeting2("Radek"))
+
+
 # Podałeś: Radek
 # dekorator -> PODAŁEŚ: RADEK
+
+@color_decorator
+def greeting3(string):
+    return f"Podałeś: {string}"
+
+
+print(greeting3("Python"))
+# Podałeś: Python
